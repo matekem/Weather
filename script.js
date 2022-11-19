68e194a0d428241ad42675a8942d5a2552a5e08a
matekem <97744782+matekem@users.noreply.github.com>
2022-11-19 17:46:43 +0100

    Testing x2

--- script.js
+++ script.js
@@ -183,13 +183,13 @@ function setBackground(forecastObject){
             var sunsetTime = Date.parse(forecastObject.forecast.forecastday[0].date  + " " + forecastObject.forecast.forecastday[0].astro.sunset)
             var sunriseTime = Date.parse(forecastObject.forecast.forecastday[0].date  + " " + forecastObject.forecast.forecastday[0].astro.sunrise)
             var currentTime = Date.parse(today)
-            var isNight = currentTime >= sunsetTime || currentTime <= sunriseTime
+            var isNight = currentTime - sunsetTime >= 0 || currentTime - sunriseTime <= 0
             console.log(isNight)
             if(isNight){
-                /*docBody.style.background = "rgb(53,0,139)" ;
-                docBody.style.background = "linear-gradient(180deg, rgba(53,0,139,1) 33%, rgba(21,0,98,1) 74%)";*/
+                docBody.style.background = "rgb(53,0,139)" ;
+                docBody.style.background = "linear-gradient(180deg, rgba(53,0,139,1) 33%, rgba(21,0,98,1) 74%)";
 
-                docBody.style.background = "blue"
+                
             }
             else{
                 switch(forecastObject.current.condition.text)
